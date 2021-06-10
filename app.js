890e15767af67299a3887409cdd811feff3ebfe5
CyrilBaah <cyril.baah@amalitech.org>
2021-06-10 13:05:40 +0000

    Quantity

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+let quantityValue = parseInt(document.getElementById("quantity").value);
+
+
 closeSlider = () => {
     document.getElementById('slider').style.width = 0;
 }
 
 openSlider = () => {
     document.getElementById('slider').style.width = "20em";
+}
+
+decreaseQuantity = () => {
+    if (quantityValue > 1) {
+        quantityValue -= 1;
+        document.getElementById("quantity").value = quantityValue;
+    }
+}
+
+increaseQuantity = () => {
+    if (quantityValue < 100) {
+        quantityValue += 1;
+        document.getElementById("quantity").value = quantityValue;
+    }
 }
