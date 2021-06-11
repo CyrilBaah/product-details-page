b3bade17f68bf805063ebf675cc7a65d3c065663
CyrilBaah <cyril.baah@amalitech.org>
2021-06-11 14:37:16 +0000

    delete Button

--- cart.js
+++ cart.js
@@ -15,17 +15,26 @@ document.getElementById('cart-productimage').src = productImage;
 document.getElementById('cart-productsize').innerHTML = productSize;
 document.getElementById('cart-unitprice').innerHTML = productQuantity * productPrice;
 document.getElementById('cart-totalprice').innerHTML = productQuantity * productPrice;
+document.getElementById('checkout-subtotal').innerHTML = productQuantity * productPrice;
+document.getElementById('checkout-total').innerHTML = productQuantity * productPrice;
 
 updatePrice = (value) => {
     const productPrice = 70;
     document.getElementById('cart-unitprice').innerHTML = value * productPrice;
     document.getElementById('cart-totalprice').innerHTML = value * productPrice;
+    document.getElementById('checkout-total').innerHTML = value * productPrice;
+    document.getElementById('checkout-subtotal').innerHTML = value * productPrice;
 }
 
 updateQuantity = () => {
     let productQuantity = document.getElementById('cart-productquantity').value;
     // console.log(productQuantity);
     updatePrice(productQuantity);
+}
+
+deleteCart = () => {
+    document.getElementById('cart-delete').remove();
+
 }
 
 
