3e9685392ff37975ce9da0500f8006a5ffa49fcd
CyrilBaah <cyril.baah@amalitech.org>
2021-06-12 14:18:59 +0000

    refactor

--- cart.js
+++ cart.js
@@ -40,62 +40,24 @@ deleteCart = () => {
     document.getElementById('checkout-discount').innerHTML = "";
     document.getElementById('checkout-subtotal').innerHTML = "";
     document.getElementById('checkout-total').innerHTML = "";
+    document.getElementById('checkout-marquee').innerHTML = "";
+    document.getElementById('coupon').value = "";
 
-}
-
-discountCoupon = (code) => {
-    const codeTwentyPercent = "20Percent%";
-    const codeThirtyPercent = "30Percent%";
-    const codeFortyPercent = "40Percent%";
-    const codeFiftyPercent = "50Percent%";
-
-    const codeTwentyPercentValue = 0.2;
-    const codeThirtyPercentValue = 0.3;
-    const codeFortyPercentValue = 0.4;
-    const codeFiftyPercentValue = 0.5;
-
-    let subTotal = document.getElementById('checkout-subtotal').innerHTML;
-    let twentyTotal = subTotal - (subTotal * codeTwentyPercentValue);
-    let thirtyTotal = subTotal - (subTotal * codeThirtyPercentValue);
-    let fortyTotal = subTotal - (subTotal * codeFortyPercentValue);
-    let fiftyTotal = subTotal - (subTotal * codeFiftyPercentValue);
-
-    switch (code) {
-        case codeTwentyPercent:
-            document.getElementById('checkout-subtotal').innerHTML = subTotal;
-            document.getElementById('checkout-discount').innerHTML = "20%";
-            document.getElementById('checkout-total').innerHTML = twentyTotal;
-            document.getElementById('checkout-marquee').innerHTML = `<marquee direction="">Amount to be paid is $${twentyTotal}</marquee>`;
-            break;
-
-        case codeThirtyPercent:
-            document.getElementById('checkout-subtotal').innerHTML = subTotal;
-            document.getElementById('checkout-discount').innerHTML = "30%";
-            document.getElementById('checkout-total').innerHTML = thirtyTotal;
-            document.getElementById('checkout-marquee').innerHTML = `<marquee direction="">Amount to be paid is $${thirtyTotal}</marquee>`;
-            break;
-
-        case codeFortyPercent:
-            document.getElementById('checkout-subtotal').innerHTML = subTotal;
-            document.getElementById('checkout-discount').innerHTML = "40%";
-            document.getElementById('checkout-total').innerHTML = fortyTotal;
-            document.getElementById('checkout-marquee').innerHTML = `<marquee direction="">Amount to be paid is $${fortyTotal}</marquee>`;
-            break;
-
-        case codeFiftyPercent:
-            document.getElementById('checkout-subtotal').innerHTML = subTotal;
-            document.getElementById('checkout-discount').innerHTML = "50%";
-            document.getElementById('checkout-total').innerHTML = fiftyTotal;
-            document.getElementById('checkout-marquee').innerHTML = `<marquee direction="">Amount to be paid is $${fiftyTotal}</marquee>`;
-            break;
-
-        default:
-            document.getElementById('checkout-marquee').innerHTML = `<marquee direction="">Invalid Code</marquee>`;
-            break;
-    }
 }
 
 discount = () => {
+    const codeFiftyPercentValue = 0.5;
+    let subTotal = document.getElementById('checkout-subtotal').innerHTML;
+    let fiftyTotal = subTotal - (subTotal * codeFiftyPercentValue);
+
     let discountCode = document.getElementById('coupon').value;
-    discountCoupon(discountCode);
+    if (discountCode.length > 5) {
+        document.getElementById('checkout-subtotal').innerHTML = subTotal;
+        document.getElementById('checkout-discount').innerHTML = "50%";
+        document.getElementById('checkout-total').innerHTML = fiftyTotal;
+        document.getElementById('checkout-marquee').innerHTML = `<marquee direction="">Amount to be paid is $${fiftyTotal}</marquee>`;
+    } else {
+        document.getElementById('checkout-marquee').innerHTML = `<marquee direction="">Invalid Code</marquee>`;
+    }
+
 }
